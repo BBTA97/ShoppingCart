6d69aef0c30c9dc7433898e9125defdc01c43499
BBTA97 <patcharada.kh@ku.th>
2025-07-16 21:51:52 +0700

    Add files via upload

--- Lib/CartItem.java
+++ Lib/CartItem.java
@@ -1,2 +1,3 @@
+package Lib;
 public record CartItem(String sku, String name, double price, int quantity){
 }
--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import Lib.ShoppingCartManualTest;
 class Main{
     public static void main(String[] args){
         ShoppingCartManualTest.run();
